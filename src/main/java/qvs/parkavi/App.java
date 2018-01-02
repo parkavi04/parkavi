59a95722d5efa6aea052877d815b0f4d54c5cef2
parkavi04 <parkavi.umapathy@sensiple.com>
2018-01-02 17:07:43 +0530

    Initial commit

---
+++ src/main/java/qvs/parkavi/App.java
@@ -0,0 +1,13 @@
+package qvs.parkavi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
